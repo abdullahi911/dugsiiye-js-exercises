f70c6daade89bd00d3ffe85832bb2854b1a4df0c
Abdullahi Abdikari <abdullahi@example.com>
2025-11-11 23:47:07 +0300

    submitting exercise3 in javascript

---
+++ exercise3 javascript/exe3.js
@@ -0,0 +1,8 @@
+let isSUnny = true;
+let isWeekend = true;
+console.log("isSUnny && isWeekend",isSUnny && isWeekend
+ );
+
+console.log("isSUnny || isWeekend",isSUnny || isWeekend);
+
+console.log("|isSUnny",!isSUnny );
