e592a44450660a51cd44f35698bc935b1b56cb6a
Abdullahi Abdikari <abdullahi@example.com>
2025-11-12 22:28:15 +0300

    submitting exercise 4

---
+++ exercise4 javascript/ex4.js
@@ -0,0 +1,16 @@
+function add (number1,number2){
+    return number1 + number2;
+}
+console.log("returning add function waakan")
+console.log(add(5,10));
+console.log(add(9,90));
+console.log(add(5,10));
+
+let addt = function(number1, number2){
+  console.log(number1 + number2);
+
+}
+console.log("function expression waakan");
+addt(8,10);
+addt(90,10);
+addt(5,15);
