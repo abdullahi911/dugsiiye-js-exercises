9eacc5440b2f2e835c1d75d94be9dea16eca816c
Abdullahi Abdikari <abdullahi@example.com>
2025-11-13 10:10:24 +0300

    submitting exercise 10

---
+++ exercise10 javascript/exe10.js
@@ -0,0 +1,16 @@
+let library = [
+  {
+
+    title:'javascript for beginners',
+    outhor: 'ustaad hamuda',
+    year: 2025,
+  },
+    {
+
+    title:'html for beginners',
+    outhor: 'ustaad omar tood',
+    year: 2020,
+  },
+]
+console.log("first book title:",library[0].title);
+console.log("second book outhor:",library[1].outhor);
