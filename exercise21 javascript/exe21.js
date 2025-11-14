0d0a56e80ad5256d236f3225eda3a468bf3512b0
Abdullahi Abdikari <abdullahi@example.com>
2025-11-15 00:09:38 +0300

    submitting assigmnet 21

---
+++ exercise21 javascript/exe21.js
@@ -0,0 +1,5 @@
+let num = [1,2,3,4,5];
+console.log("nambarrada asalka ah:",num);
+let newnum = num.reduce((Result,myltipler) => Result * myltipler,1
+);
+console.log("jawaabta markaan reduce kusamayeey:",newnum);
