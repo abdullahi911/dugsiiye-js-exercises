5219d00c704a98442bc2338a09277c26c80b7567
Abdullahi Abdikari <abdullahi@example.com>
2025-11-15 16:31:13 +0300

    exercise 26 submitting

---
+++ exercise26 javascript/exe26.js
@@ -0,0 +1,12 @@
+function blocking(){
+  alert("shaqadanaa kahoraysa in laguu dabaco wax");
+  console.log("waxaa laguu dabacay kadib alert ok siisay");
+}
+blocking();
+function unblocking(callback){
+  setTimeout(() => { 
+  console.log ("labo daqiiqo kadib soobax");}, 2000);
+  console.log("soobax kahor settimeout");
+}
+unblocking();
+
