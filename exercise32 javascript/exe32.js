69c1b07e6a28c5e8e6a8599b479e4bb643643ce0
Abdullahi Abdikari <abdullahi@example.com>
2025-11-17 11:52:06 +0300

    soogudbinta exercise 32

---
+++ exercise32 javascript/exe32.js
@@ -0,0 +1,8 @@
+console.log("querySelector ")
+const image = document.querySelector('.image');
+console.log(image);
+console.log("using querySelectorAll to select all buttons:")
+let buttons = document.querySelectorAll('button');
+console.log(buttons);
+let orders = document.querySelectorAll('ol');
+console.log(orders);
